c69477fe73f77947cf55ae746fe737689582e0e5
Norton Allen <allen@huarp.harvard.edu>
1992-07-10 19:31:48 +0000

    More complexity requiring compact model cmdgen (revealed a few more bugs)

--- cmdgen/trial.cmd
+++ cmdgen/trial.cmd
@@ -4,6 +4,8 @@
   #define DCV_LmpAOn 0
   #define DCV_LmpBOn 2
   #define DCV_HtrOn 4
+  #define IOMODE_INIT (IO_SPACE|IO_BACKSPACE)
+  #define QNX_CONS
   void data(short type, short val) {
 	printf("\nExecuting data command %d %d\n", type, val);
   }
@@ -12,13 +14,16 @@
 	;
 &commands :
 	: &commands &command
+	: &commands Other * &othercmds
 	;
 &command
 	: *
-	: &on_off_cmd &on_or_off * { data(DCT_STR, $1 + $2); }
+	: &on_off_cmd &on_or_off * {
+		data(DCT_STR, $1 + $2);
+		printf("TSP = %d, vsp = %d\n", tsp, vsp);
+	  }
 	: Data %d (Command type) %d (Command Value) * { data($2, $3); }
 	: IOMODE %d (BS=1 SP=2 AL=4 WD=8) * { iomode = $2; }
-	: Other * &othercmds
 	;
 &on_off_cmd <int>
 	: Lamp A { $0 = DCV_LmpAOn; }
@@ -40,10 +45,24 @@
 	: a *
 	: b *
 	: c *
+	: Root &command
 	;
 %%
 
-int main(void) {
+int main(int argc, char **argv) {
+  #ifdef QNX_CONS
+	int i;
+  
+	for (i = 1; i < argc; i++) {
+	  if (define_con(argv[i]))
+		fprintf(stderr, "Error opening console %s\n", argv[i]);
+	}
+  #endif
+  
   command();
+  
+  #ifdef QNX_CONS
+	close_cons();
+  #endif
   return(0);
 }
