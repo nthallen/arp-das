adf010c0b278f06c69a0edae32c75da955abfb39
Norton Allen <allen@huarp.harvard.edu>
1992-09-09 18:45:23 +0000

    Latest version

--- nortlib2/src/nortlib.h
+++ nortlib2/src/nortlib.h
@@ -1,15 +1,31 @@
 /* nortlib.h include file for nortlib
  * $Log$
+ * Revision 1.1  1992/08/25  15:31:42  nort
+ * Initial revision
+ *
  */
 #ifndef _NORTLIB_H_INCLUDED
 #define _NORTLIB_H_INCLUDED
-#ifndef FILENAME_MAX
-  #error Must include stdio.h before nortlib.h
-#endif
+
+#include <stdio.h>
+#include <sys/types.h>
 
 int Skel_open(char *name);
 int Skel_copy(FILE *ofp, char *label, int copyout);
 
 extern int (*nl_error)(unsigned int level, char *s, ...);
 int nl_err(unsigned int level, char *s, ...);
+extern int nl_response; /* nlresp.c */
+int set_response(int newval); /* nlresp.c */
+#define NLRSP_DIE 3
+#define NLRSP_WARN 1
+#define NLRSP_QUIET 0
+
+pid_t find_DG(void); /* find_dg.c */
+int send_DG(void *msg, int size); /* send_dg.c */
+pid_t find_CC(int dg_ok); /* find_cc.c */
+int send_CC(void *msg, int size, int dg_ok); /* send_cc.c */
+int send_dascmd(int type, int value, int dg_ok); /* senddasc.c */
+int reply_byte(pid_t sent_pid, unsigned char msg); /* repbyte.c */
+
 #endif
