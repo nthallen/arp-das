e134c91609bc2b5d7e967348712c183346b7c7f7
Norton Allen <allen@huarp.harvard.edu>
1993-07-01 15:30:23 +0000

    Initial revision

---
+++ nortlib2/src/nl_verr.c
@@ -0,0 +1,36 @@
+/* nl_verr.c contains nl_verror() which allows easy expansion of
+ * the nl_error capabilities in many cases.
+ * $Log$
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "nortlib.h"
+#ifdef __WATCOMC__
+  #pragma off (unreferenced)
+	static char rcsid[] =
+	  "$Id$";
+  #pragma on (unreferenced)
+#endif
+
+int nl_verror(FILE *ef, int level, const char *fmt, va_list args) {
+  char *lvlmsg;
+
+  if (level < -1 && nl_debug_level > level) return(level);
+  switch (level) {
+	case -1: lvlmsg = ""; break;
+    case 0: lvlmsg = ""; break;
+	case 1: lvlmsg = "Warning: "; break;
+	case 2: lvlmsg = "Error: "; break;
+	case 3: lvlmsg = "Fatal: "; break;
+	default:
+	  if (level <= -2) lvlmsg = "Debug: ";
+	  else lvlmsg = "Internal: ";
+	  break;
+  }
+  fprintf(ef, "%s", lvlmsg);
+  vfprintf(ef, fmt, args);
+  fputc('\n', ef);
+  if (level > 2 || level == -1) exit(level > 0 ? level : 0);
+  return(level);
+}
---
+++ nortlib2/src/nldbg.c
@@ -0,0 +1,12 @@
+/* nldbg.c defines nl_debug_level
+ * $Log$
+ */
+#include "nortlib.h"
+#ifdef __WATCOMC__
+  #pragma off (unreferenced)
+	static char rcsid[] =
+	  "$Id$";
+  #pragma on (unreferenced)
+#endif
+
+int nl_debug_level = -1;
