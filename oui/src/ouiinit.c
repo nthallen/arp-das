f4ebcf6e8db813bba12e56fc2ccc18c61dc6ad02
Norton Allen <allen@huarp.harvard.edu>
1994-09-16 15:09:59 +0000

    Initial revision

---
+++ oui/src/ouiinit.c
@@ -0,0 +1,43 @@
+/* OUI output from the following packages:
+   oui
+   output_ext_init
+   compiler
+*/
+char *opt_string = "uqkwvo:";
+#include "oui.h"
+#include "ouidefs.h"
+#include <unistd.h>
+#include "nortlib.h"
+#include "compiler.h"
+int (*nl_error)(int level, char *s, ...) = compile_error;
+
+void oui_init_options(int argc, char **argv) {
+  char *output_extension;
+
+  { int optltr;
+
+	optind = 0;
+	opterr = 0;
+	while ((optltr = getopt(argc, argv, opt_string)) != -1) {
+	  switch (optltr) {
+		case 'u': sort_output = 0; break;
+		case '?':
+		  nl_error(3, "Unrecognized Option -%c", optopt);
+		default:
+		  break;
+	  }
+	}
+  }
+  output_extension = ".c";
+  compile_init_options(argc, argv, output_extension);
+}
+
+#ifdef __USAGE
+%C	[options] file [file ...]
+	-k Keep incomplete output file on error
+	-o <filename> Specify Output Filename
+	-q Print usage message
+	-u Don't sort the "sort" strings
+	-v Increasing level of verbosity
+	-w Treat warnings as errors
+#endif
