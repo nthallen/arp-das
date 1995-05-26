bb10bad000642a56d285173cb1747a05b6c135be
Norton Allen <allen@huarp.harvard.edu>
1995-05-26 14:44:45 +0000

    Added opt_string definition!

--- oui/src/oui.h
+++ oui/src/oui.h
@@ -1,4 +1,8 @@
 /* oui.h provides the prototype for oui_init_options()
  * $Log$
+ * Revision 1.1  1994/11/22  14:54:36  nort
+ * Initial revision
+ *
  */
 void oui_init_options(int argc, char **argv);
+extern char *opt_string;
