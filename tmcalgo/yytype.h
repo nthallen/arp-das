fe581b974ea5a92aa1bc1b475f51fa66bbe098b3
Norton Allen <allen@huarp.harvard.edu>
2001-01-23 16:30:44 +0000

    Added additionall CMDTYPE_'s and documentation

--- tmcalgo/yytype.h
+++ tmcalgo/yytype.h
@@ -1,5 +1,8 @@
 /* yytype.h
  * $Log$
+ * Revision 1.5  2001/01/05 21:55:16  nort
+ * Added argument to get_state_case()
+ *
  * Revision 1.4  1999/12/03 16:46:11  nort
  * Added NoLog option to state definition
  *
@@ -17,7 +20,7 @@
 #define _YYTYPE_H
 
 struct substate {
-  struct substate *next;
+  /* struct substate *next; */
   char *name;
   int once;
   int state_case;
@@ -32,6 +35,7 @@ struct cmddef {
   long int timeout;
   struct cmddef *else_stat;
   int cmdtype;
+  int else_count;
 };
 /* cmdtype values: */
 #define CMDTYPE_TMC  1
@@ -40,14 +44,27 @@ struct cmddef {
 #define CMDTYPE_VAL  4
 #define CMDTYPE_HOLD 5
 #define CMDTYPE_VHOLD 6
+#define CMDTYPE_HOLDV 7
+#define CMDTYPE_RES 8
+#define CMDTYPE_SS 9
 /* for CMDTYPE_TMC, cmd2text is a tmc statement, and cmdtext
    is an optional list of dependencies.
    For CMDTYPE_QSTR cmdtext is the quoted string.
    For CMDTYPE_CMD cmdtext is the command.
    For CMDTYPE_VAL cmdtext is the variable to be validated.
    For CMDTYPE_HOLD cmdtext is the expression to hold for.
+     Hold until (expr)
    For CMDTYPE_VHOLD cmdtext is the validatation list.
+	 Hold until valid (list)
+   For CMDTYPE_HOLDV cmdtext is optional state to validate
+	 Hold [and Validate <NAME>]. substate is set in
+	 list_substates if required.
+   For CMDTYPE_RES cmdtext is state to resume
+	 Resume <NAME> ;
    Types _TMC, _HOLD and _VHOLD require substate generation.
+   CMDTYPE_SS substate holds the substate to generate. If
+     cmdtext is non-zero, it is a masquerading _TMC in
+	 an else clause.
 */
 
 struct cmdlst {
@@ -110,5 +127,6 @@ void get_version(FILE *ofp); /* commands.c */
 struct substate *new_substate( FILE *ofp, char *name, int once, int mkcase );
 int get_state_case( char *statename, int slurp, int partno );
 void output_mainloop( FILE *ofp ); /* states.c */
+struct cmddef *new_command(int type, char *text, char *text2 );
 
 #endif
