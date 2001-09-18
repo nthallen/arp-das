38c7987620b8cd1741bea1c12f9aa90b25b4df3a
Norton Allen <allen@huarp.harvard.edu>
2001-09-18 15:02:35 +0000

    phtable first compiled.

--- table/phparam.c
+++ table/phparam.c
@@ -2,7 +2,10 @@
 #include "param.h"
 #include "tablelib.h"
 
+#define MAX_FIELD_WIDTH 150
+
 int ColSpace = 4;
+int BaselineSkip = 15;
 
 dims_t CalcWordDims(char *text, int attr) {
   dims_t wdims;
@@ -10,23 +13,34 @@ dims_t CalcWordDims(char *text, int attr) {
   char *font;
 
   switch (attr) {
-    case 2: font = fieldfont; break;
-	
+    case 2: font = tbl_fieldfont; break;
+    default:
+	case 4: font = tbl_labelfont; break;
   }
   tbl_ExtentText( font, text, &extent );
-  wdims.Width.Space = strlen(text);
+  wdims.Width.Space = extent.lr.x - extent.ul.x;
   wdims.Width.Glue = 0;
-  wdims.Height.Space = 1;
+  wdims.Height.Space = extent.ul.y - extent.lr.y;
   wdims.Height.Glue = 0;
+  if ( wdims.Height.Space < BaselineSkip )
+    wdims.Height.Space = BaselineSkip;
   return wdims;
 }
 
 int DatumWidth(int ncols) {
-  return ncols;
+  char buf[MAX_FIELD_WIDTH+1];
+  int i;
+  PhRect_t extent;
+
+  if ( ncols > MAX_FIELD_WIDTH ) ncols = MAX_FIELD_WIDTH;
+  for ( i = 0; i < ncols; i++ ) buf[i] = ' ';
+  buf[ncols] = '\0';
+  tbl_ExtentText( tbl_labelfont, buf, &extent );
+  return extent.lr.x - extent.ul.x;
 }
 
 int DatumHeight(int nrows) {
-  return nrows;
+  return nrows * BaselineSkip;
 }
 
-int RuleThickness = 1;
+int RuleThickness = 5;
