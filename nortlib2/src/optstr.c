8c1ab4a083d04cee2bedd9729d185fa2c54ee3da
Norton Allen <allen@huarp.harvard.edu>
2001-10-25 13:39:41 +0000

    Added some dummy definition files (yyin.c, optstr.c) to help dynamic links of unrelated programs. Added md5 implementation from RFC 1321

---
+++ nortlib2/src/optstr.c
@@ -0,0 +1,2 @@
+/* Copyright 2001 by the President and Fellows of Harvard College */
+char *opt_string = "";
---
+++ nortlib2/src/yyin.c
@@ -0,0 +1,4 @@
+/* Copyright 2001 by the President and Fellows of Harvard College */
+#include <stdio.h>
+
+FILE *yyin;
