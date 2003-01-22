b136344e6f1f0a3b350ab586f8f5482a3ac632ad
Norton Allen <allen@huarp.harvard.edu>
2003-01-22 20:07:34 +0000

    Decide resolution

--- drivers/QCLI/qclicomp/gcd.c
+++ drivers/QCLI/qclicomp/gcd.c
@@ -1,6 +1,6 @@
 #include "qclicomp.h"
 
-long int gcd( long int a, long int b ) {
+long gcd( long a, long b ) {
   long int r;
   if ( a < 0 ) a = -a;
   if ( b < 0 ) b = -b;
--- drivers/QCLI/qclicomp/qclicomp.h
+++ drivers/QCLI/qclicomp/qclicomp.h
@@ -1,6 +1,14 @@
 #ifndef QCLICOMP_H_INCLUDED
 #define QCLICOMP_H_INCLUDED
+#include <math.h>
+#include "err.h"
 
-extern long int gcd( long int, long int );
+extern long gcd( long, long );
+extern long PickRes( long Tcyclei, long Tstepi, CoordPtr pos );
+
+#define usecs(x) ((long)floor((x)*1e6+.5))
+#define DIV_UP(x,y) (((x)+(y)-1)/(y))
+#define TZI_DEFAULT 10000
+#define N_STEPS_MAX 1000
 
 #endif
