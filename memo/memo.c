d069f42cfd0f7e89b57518aa509cdb7c271ed641
Norton Allen <allen@huarp.harvard.edu>
2008-07-02 20:29:55 +0000

    Simple cleanup

--- memo/memo.c
+++ memo/memo.c
@@ -5,11 +5,10 @@
 #include <unistd.h>
 #include "oui.h"
 #include "memo.h"
+#include "tm.h"
 
 //int io_read (resmgr_context_t *ctp, io_read_t *msg, RESMGR_OCB_T *ocb);
 int io_write(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb);
-//int iofunc_open_hook( resmgr_context_t *ctp, io_open_t *msg,
-//                      RESMGR_HANDLE_T *handle, void *extra);
 
 static resmgr_connect_funcs_t    connect_funcs;
 static resmgr_io_funcs_t         io_funcs;
@@ -40,23 +39,11 @@ static iofunc_funcs_t ocb_funcs = { /* our ocb allocating & freeing functions */
 /* the mount structure, we have only one so we statically declare it */
 iofunc_mount_t mountpoint = { 0, 0, 0, 0, &ocb_funcs };
 
-// int
-// timer_tick(message_context_t *ctp, int code, unsigned flags, void *handle) {
-//     /* union sigval value = ctp->msg->pulse.value; */
-//     attr.nbytes += 12;
-//     run_readq();
-//     return 0;
-// }
-
 main(int argc, char **argv) {
     /* declare variables we'll be using */
-    int use_threads = 0;
     resmgr_attr_t        resmgr_attr;
     dispatch_t           *dpp;
     int                  id;
-    // struct sigevent      event;
-    // struct _itimer       itime;
-    // int                  timer_id;
 
     //oui_init_options( argc, argv );
 
@@ -89,7 +76,7 @@ main(int argc, char **argv) {
     /* attach our device name */
     id = resmgr_attach(dpp,            /* dispatch handle        */
                        &resmgr_attr,   /* resource manager attrs */
-                       "/dev/huarp/test/lgr",  /* device name            */
+                       tm_dev_name("memo"),  /* device name            */
                        _FTYPE_ANY,     /* open type              */
                        0,              /* flags                  */
                        &connect_funcs, /* connect routines       */
@@ -100,81 +87,26 @@ main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    /* Initialize an event structure, and attach a pulse to it */
-    // if((event.sigev_code = pulse_attach(dpp, MSG_FLAG_ALLOC_PULSE, 0, &timer_tick,
-    //                                    NULL)) == -1) {
-    //    fprintf(stderr, "Unable to attach timer pulse.\n");
-    //     return EXIT_FAILURE;
-    //}
-
-    /* Connect to our channel */
-    // if((event.sigev_coid = message_connect(dpp, MSG_FLAG_SIDE_CHANNEL)) == -1) {
-    //     fprintf(stderr, "Unable to attach to channel.\n");
-    //     return EXIT_FAILURE;
-    // }
-    // 
-    // event.sigev_notify = SIGEV_PULSE;
-    // event.sigev_priority = -1;
-    // /* We could create several timers and use different sigev values for each */
-    // event.sigev_value.sival_int = 0;
-    // 
-    // if((timer_id = TimerCreate(CLOCK_REALTIME, &event)) == -1) {;
-    //     fprintf(stderr, "Unable to attach channel and connection.\n");
-    //     return EXIT_FAILURE;
-    // }
-    // 
-    // /* And now setup our timer to fire every second */
-    // itime.nsec = 1000000000;
-    // itime.interval_nsec = 1000000000;
-    // TimerSettime(timer_id, 0,  &itime, NULL);
-
-    if ( use_threads ) {
-      thread_pool_attr_t   pool_attr;
-      thread_pool_t        *tpp;
-
-      /* initialize thread pool attributes */
-      memset(&pool_attr, 0, sizeof pool_attr);
-      pool_attr.handle = dpp;
-      pool_attr.context_alloc = dispatch_context_alloc;
-      pool_attr.block_func = dispatch_block;
-      pool_attr.handler_func = dispatch_handler;
-      pool_attr.context_free = dispatch_context_free;
-      pool_attr.lo_water = 2;
-      pool_attr.hi_water = 4;
-      pool_attr.increment = 1;
-      pool_attr.maximum = 50;     /* allocate a thread pool handle */
-      if((tpp = thread_pool_create(&pool_attr,
-                                   POOL_FLAG_EXIT_SELF)) == NULL) {
-          fprintf(stderr, "%s: Unable to initialize thread pool.\n",
-                  argv[0]);
-          return EXIT_FAILURE;
-      }     /* start the threads, will not return */
-      thread_pool_start(tpp);
-    } else {
-      int running = 2;
-      dispatch_context_t   *ctp;
-      ctp = dispatch_context_alloc(dpp);
-      printf( "\nStarting:\n" );
-      while ( running ) {
-	if ((ctp = dispatch_block(ctp)) == NULL) {
-	  fprintf(stderr, "block error\n" );
-	  return EXIT_FAILURE;
-	}
-	// printf( "  type = %d,%d  attr.count = %d\n",
-	//   ctp->resmgr_context.msg->type,
-	//   ctp->resmgr_context.msg->connect.subtype, attr.count );
-	dispatch_handler(ctp);
-	if ( running > 1 && attr.count > 0 ) running = 1;
-	else if ( running == 1 && attr.count == 0 ) running = 0;
-      }
-      printf( "Terminating\n" );
+		// running starts at 2. When the first client opens a connection,
+	  // we change to 1. When all connections are closed, we go to 0.
+	  // In this way, we don't require any additional commanding, but
+	  // we do require that there be some continuity in how the clients
+	  // use the resource.
+    int running = 2;
+    dispatch_context_t   *ctp;
+    ctp = dispatch_context_alloc(dpp);
+    printf( "\nStarting:\n" );
+    while ( running ) {
+			if ((ctp = dispatch_block(ctp)) == NULL) {
+			  fprintf(stderr, "block error\n" );
+			  return EXIT_FAILURE;
+			}
+			dispatch_handler(ctp);
+			if ( running > 1 && attr.count > 0 ) running = 1;
+			else if ( running == 1 && attr.count == 0 ) running = 0;
     }
+    printf( "Terminating\n" );
 }
-
-// int iofunc_open_hook( resmgr_context_t *ctp, io_open_t *msg,
-//                       RESMGR_HANDLE_T *handle, void *extra) {
-//   iofunc_open_default( ctp, msg, handle, extra );
-// }
 
 #define LGR_BUF_SIZE 70
 
