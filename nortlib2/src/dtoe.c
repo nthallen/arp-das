920603f7e5610a57c9fbc0b47f82b48ebc62ad7e
Norton Allen <allen@huarp.harvard.edu>
2008-07-03 20:56:51 +0000

    Remove reference to ecvt() (legacy Watcom function) Replaced with slower snprintf invocation.

--- nortlib2/src/dtoe.c
+++ nortlib2/src/dtoe.c
@@ -8,25 +8,23 @@ char *dtoe( double v, int width, char *obuf ) {
   char *buf;
   int dec, sign, i, j;
 
-  if ( obuf == 0 ) obuf = sbuf;
-  /* Subtract 6 from width to represent sign,decimal,E,sign,exponent
-	 (assuming only two digits for the exponent */
-  buf = ecvt( v, width-6, &dec, &sign );
-  dec--;
-  j = 0;
-  obuf[j++] = sign ? '-' : ' ';
-  obuf[j++] = buf[0];
-  obuf[j++] = '.';
-  for ( i = 1; buf[i] != 0; )
-	obuf[j++] = buf[i++];
-  obuf[j++] = 'E';
-  if ( dec >= 0 ) obuf[j++] = '+';
-  else {
-	obuf[j++] = '-';
-	dec = -dec;
+  if ( obuf == 0 ) {
+    obuf = sbuf;
+    if ( width > 29 ) width = 29;
   }
-  obuf[j++] = dec / 10 + '0';
-  obuf[j++] = dec % 10 + '0';
-  obuf[j++] = '\0';
+  if ( width < 7 ) snprintf( obuf, width+1, "*******" );
+  else {
+    int nn;
+    int prec = width-7;
+    nn = snprintf( obuf, width+1, "% #*.*e", width, prec, v );
+    if ( nn == width+1 ) {
+      if ( prec > 0 ) {
+        nn = snprintf( obuf, width+1, "% #*.*e", width-1, prec-1, v );
+      } else {
+        nn = snprintf( obuf, width+1, "% *.*e", width-1, prec, v );
+      }
+    }
+    if ( nn != width ) obuf[0] = '!';
+  }
   return obuf;
 }
