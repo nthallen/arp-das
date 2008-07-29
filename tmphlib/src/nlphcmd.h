6bcf7277e29d420ea231d479ddf8e0750bc6e087
Norton Allen <allen@huarp.harvard.edu>
2008-07-29 19:22:36 +0000

    extern "C" declaration

--- tmphlib/src/nlphcmd.h
+++ tmphlib/src/nlphcmd.h
@@ -1,8 +1,17 @@
 #ifndef CMDCLT_H_INCLUDED
 #define CMDCLT_H_INCLUDED
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Header "nlphcmd.h" for cltwindow Application */
 extern int nlph_getch(void);
 extern void nlph_cmdclt_init( void *(*cmd_thread)(void*));
 extern void nlph_update_cmdtext( char *cmdtext, char *prompttext );
+
+#ifdef __cplusplus
+};
+#endif
+
 #endif
