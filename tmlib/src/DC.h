6a01a4c0b4ef3d7237486c938cbb1100af26adb9
Norton Allen <allen@huarp.harvard.edu>
2008-07-31 15:55:32 +0000

    Make process_data() pure virtual

--- tmlib/src/DC.h
+++ tmlib/src/DC.h
@@ -9,7 +9,7 @@ class data_client {
     void operate(); // event loop
     static unsigned int next_minor_frame, majf_row, minf_row;
   protected:
-    virtual void process_data();
+    virtual void process_data() = 0;
     virtual void process_init();
     virtual void process_tstamp();
     int bfr_fd;
