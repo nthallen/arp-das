d1a19ccf9bd042315c9727d6531b3ff3ac05a950
Norton Allen <allen@huarp.harvard.edu>
2008-08-01 15:09:05 +0000

    Added table name functionality

--- table/table_name.c
+++ table/table_name.c
@@ -1,13 +1,14 @@
-#include "csm.h"
+#include "table_name.h"
 
 /* get_table_name() strips the extension from the input file name.
    Returns an index into the string table
  */
-int get_table_name(void) {
+int get_table_name(DefTableKey InputFile) {
   char *fname;
   int i;
-  if ( InputFile == NoKey ) return NoStr;
+  if ( InputFile == NoKey )
+    return NoStrIndex;
   fname = StringTable(GetClpValue(InputFile,0));
   for ( i = 0; fname[i] != '\0' && fname[i] != '.'; i++);
-  return StoStr(fname,i);
+  return stostr(fname,i);
 }
--- table/table_name.h
+++ table/table_name.h
@@ -1,2 +1,9 @@
 /* table_name.h */
-int get_table_name(void);
+#ifndef TABLE_NAME_H
+#define TABLE_NAME_H
+
+#include "csm.h"
+#include "pdl_gen.h"
+int get_table_name(DefTableKey InputFile);
+
+#endif
