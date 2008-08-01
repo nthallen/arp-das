dfb5a540e7cd745c4f70e5cdb0be1681fdac0dc0
Norton Allen <allen@huarp.harvard.edu>
2008-08-01 15:04:45 +0000

    Sort out libraries to avoid including c++ libs where not needed

--- tmutil/appgen.mk
+++ tmutil/appgen.mk
@@ -1,28 +1,30 @@
 # make include files for appgen QNX6 output.
-CFLAGS=
-LDFLAGS=
-LIBS=-l tmph -l tm -l nort
-LINK.priv=/bin/rm -f $@; $(LINK.c) $(LIBFLAGS) -T 1 -o $@ $(LDFLAGS)
-LINK.norm=$(LINK.c) $(LIBFLAGS) -o $@ $(LDFLAGS)
+CPPFLAGS=-I/usr/local/include
+CXX=cc
+CXXFLAGS=-lang-c++ -I/usr/local/include
+AG_LDFLAGS=-L/usr/local/lib -Wl,-rpath -Wl,/usr/local/lib
+LINK.norm=$(CC) $(CPPFLAGS) $(CFLAGS) $(AG_LDFLAGS) $(LDFLAGS) -o $@
+LINK.priv=/bin/rm -f $@; $(LINK.norm)
 TMCREV=tmc
 TMC=$(TMCREV) -s -o $@ $(TMCFLAGS)
-TMC.col=name=$@; $(TMC) -p -V $${name%col.c}.pcm -c -D tm.dac $(COLFLAGS)
+TMC.col=name=$@; $(TMC) -p -V $${name%col.cc}.pcm -c -D tm.dac $(COLFLAGS)
 OUIDIR=/usr/local/share/oui
 OUI=oui -o $@
 OUIUSE=usemsg $@
 LIBSRC=/usr/local/share/huarp
 CMDGEN=cmdgen -o $@
-COMPILE.clt=$(COMPILE.c) -fo$@ -D CLIENT
-COMPILE.srvr=$(COMPILE.c) -fo$@ -D SERVER
+COMPILE.clt=$(COMPILE.c) -o $@ -D CLIENT
+COMPILE.srvr=$(COMPILE.c) -o $@ -D SERVER
 AWK=awk > $@ -f $(LIBSRC)
 FLD2DISP=$(AWK)/fld2disp.awk
 EDF2EXT=$(AWK)/edf2ext.awk
+PROMOTE=/usr/local/sbin/promote
 SLP2CMD=$(AWK)/slp2cmd.awk
 SLP2SOL=$(AWK)/slp2sol.awk
 EDF2OUI=$(AWK)/edf2oui.awk
 TMG2TMC=tmg2tmc > $@
 CYCLE=$(AWK)/cycle.awk
-TABLE=table > $@
+TABLE=phtable > $@
 DATAATTR=data_attr > $@
 SERVER=srvr() { $$1 -vsy & namewait -p $$! cmdinterp || { $$1; return 1; }; }; srvr
 TMAREV=tmcalgo
