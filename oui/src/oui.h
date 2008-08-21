d17f468f9b813e1fdf43ce4ca51e2ef9d413d821
Norton Allen <allen@huarp.harvard.edu>
2008-08-21 20:26:30 +0000

    Needed extern "C" for C++ compilations

--- oui/src/oui.h
+++ oui/src/oui.h
@@ -1,8 +1,19 @@
 /* oui.h provides the prototype for oui_init_options()
  * $Log$
+ * Revision 1.2  1995/05/26 14:44:45  nort
+ * Added opt_string definition!
+ *
  * Revision 1.1  1994/11/22  14:54:36  nort
  * Initial revision
  *
  */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void oui_init_options(int argc, char **argv);
 extern char *opt_string;
+
+#ifdef __cplusplus
+};
+#endif
