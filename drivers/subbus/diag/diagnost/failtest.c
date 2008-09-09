f9faab416fa776fd662aa65c358f9a320ac4a5d8
Norton Allen <allen@huarp.harvard.edu>
2008-09-09 13:20:44 +0000

    Port failtest to NTO

--- drivers/subbus/diag/diagnost/failtest.c
+++ drivers/subbus/diag/diagnost/failtest.c
@@ -1,12 +1,24 @@
 /* failtest.c
  * $Log$
+ * Revision 1.1  2008/08/24 15:35:38  ntallen
+ * Diagnostics I want to port
+ *
  * Revision 1.1  2008/07/23 13:07:49  nort
  * Uncommitted changes and imports
  *
  *
  */
 #include <stdlib.h>
-#include <conio.h>
+#include "nortlib.h"
+#ifdef __QNXNTO__
+  #include <string.h>
+  #include <errno.h>
+  #include <sys/neutrino.h>
+  #include <hw/inout.h>
+  #define outp(x,y) out8(x,y)
+#else
+  #include <conio.h>
+#endif
 char failtest_c_rcsid[] = "$Id$";
 
 #ifdef __USAGE
@@ -21,12 +33,16 @@ char failtest_c_rcsid[] = "$Id$";
 int main(int argc, char **argv) {
   int mask;
 
-  if (argc < 2) print_usage(argv);
+  if (argc < 2) nl_error( 3, "Must specify fail value" );
   else {
-	mask = atoi(argv[1]);
-	outp(0x319, 0); /* tick to turn of 2-minute failure */
-	outp(0x311, 0);
-	outp(0x317, mask & 3);
+    #ifdef __QNXNTO__
+      if (ThreadCtl(_NTO_TCTL_IO,0) == -1 )
+          nl_error( 3, "Error requesting I/O priveleges: %s", strerror(errno) );
+    #endif
+    mask = atoi(argv[1]);
+    outp(0x319, 0); /* tick to turn of 2-minute failure */
+    outp(0x311, 0);
+    outp(0x317, mask & 3);
   }
   return(0);
 }
