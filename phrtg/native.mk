4ba4d7abc9874a552cedfc618280e2ad9d4bf861
Norton Allen <allen@huarp.harvard.edu>
2009-09-14 15:55:17 +0000

    Additional notes

--- phrtg/native.mk
+++ phrtg/native.mk
@@ -3,6 +3,10 @@
 # it compiles if you give the target explicitly on the
 # command line:
 #   make -fnative.mk /home/nort/BUILD/QNX6/phrtg/x86/o-g/phrtg_g
+#
+# It is also important to make sure there is a soft link:
+#   ln -s /usr/local $QNX_TARGET/usr/local
+#
 PROJECT_ROOT=/home/nort/BUILD/QNX6/phrtg
 BUILDNAME=phrtg_g
 include Makefile
