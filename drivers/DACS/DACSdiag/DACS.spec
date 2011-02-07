508a81a891eb1f5af5ab9ac2d5bdd0b7dd6711f2
Norton Allen <allen@huarp.harvard.edu>
2011-02-07 13:46:07 +0000

    Analog Outputs

--- drivers/DACS/DACSdiag/DACS.spec
+++ drivers/DACS/DACSdiag/DACS.spec
@@ -6,8 +6,9 @@ cmdbase = cmdenbl.cmd ana_in.cmd
 cmdbase = idx64drv.cmd
 cmdbase = swstat.cmd
 cmdbase = dccc.cmd
+cmdbase = ana_out.cmd
 tmcbase = DACS.tmc idx64.tmc /usr/local/share/huarp/flttime.tmc
-tmcbase = swstat.tmc ana_in.tmc
+tmcbase = swstat.tmc ana_in.tmc ana_out.tmc
 
 SRCDIST = swstat.h
 SCRIPT = idx64.idx64 interact dccc.dccc
@@ -15,6 +16,6 @@ TGTDIR = $(TGTNODE)/home/DACS
 
 DACSsrvr : -lsubbus
 DACScol : idx64col.tmc ana_in_cfg.tmc -lsubbus
-DACSdisp : idx64flag.tmc idx64.tbl ana_in.tbl digio.tmc digio.tbl
+DACSdisp : idx64flag.tmc idx64.tbl ana_in.tbl digio.tmc digio.tbl ana_out.tbl
 DACSalgo : DACS.tma
 doit : DACS.doit
---
+++ drivers/DACS/DACSdiag/ana_out.cmd
@@ -0,0 +1,28 @@
+&command
+	: Set &ao_chan %lf (Enter voltage 0-10) * {
+	    double N = $3 * 6553.6;
+	    unsigned short bits;
+	    if (N > 65535) N = 65535;
+	    if (N < 0) N = 0.;
+	    bits = (unsigned short) N;
+	    sbwr( $2, bits );
+	  }
+	;
+&ao_chan <unsigned short>
+	: AO400 { $0 = 0x400; }
+	: AO402 { $0 = 0x402; }
+	: AO404 { $0 = 0x404; }
+	: AO406 { $0 = 0x406; }
+	: AO408 { $0 = 0x408; }
+	: AO40A { $0 = 0x40A; }
+	: AO40C { $0 = 0x40C; }
+	: AO40E { $0 = 0x40E; }
+	: AO410 { $0 = 0x410; }
+	: AO412 { $0 = 0x412; }
+	: AO414 { $0 = 0x414; }
+	: AO416 { $0 = 0x416; }
+	: AO418 { $0 = 0x418; }
+	: AO41A { $0 = 0x41A; }
+	: AO41C { $0 = 0x41C; }
+	: AO41E { $0 = 0x41E; }
+	;
