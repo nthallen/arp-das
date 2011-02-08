e69b4440c40dc07e6fd48017d5d8381a93c0acff
Norton Allen <allen@huarp.harvard.edu>
2011-02-08 16:44:12 +0000

    Analog Input Definitions

---
+++ drivers/DACS/DACSutil/ai_cfg.h
@@ -0,0 +1,15 @@
+/* ai_cfg.h */
+#ifndef AI_CFG_H_INCLUDED
+#define AI_CFG_H_INCLUDED
+
+#define AI_CFG_HI_Z  0x01
+#define AI_CFG_NULL  0x02
+#define AI_CFG_XP096 0x00
+#define AI_CFG_XP192 0x08
+#define AI_CFG_XP384 0x10
+#define AI_CFG_XP768 0x18
+#define AI_CFG_X1    0x14
+#define AI_CFG_X2    0x1C
+
+#endif
+
