927eb7b80180ba5bb3947546c52631dcbd7aed25
Norton Allen <allen@huarp.harvard.edu>
2011-02-23 22:58:43 +0000

    Preliminary port

--- drivers/soldrv/soldrv.c
+++ drivers/soldrv/soldrv.c
@@ -33,36 +33,36 @@ Ported to QNX6 by NTA 2/22/11.
 // #include "da_cache.h"
 #include "oui.h"
 
-  // #include <sys/timers.h>
+// #include <sys/timers.h>
 
-  timer_t timer=0;
-  struct itimercb tcb;
-  struct itimerspec tval, otval;
-  unsigned int SolStat = 0;
+timer_t timer=0;
+struct itimercb tcb;
+struct itimerspec tval, otval;
+unsigned int SolStat = 0;
 
-  #define DOWN_TIMER { \
-      tval.it_value.tv_sec = 0L; \
-      tval.it_value.tv_nsec = 0L; \
-      if (reltimer(timer, &tval, 0) == -1) \
-    msg(MSG_EXIT_ABNORM, "Error in reltimer"); \
-      while (Creceive(timer_proxy,0,0)!=-1); \
-      errno=0; \
-  }
-  #define UP_TIMER \
-      if (reltimer(timer, &tval, 0) == -1) \
-    msg(MSG_EXIT_ABNORM, "Error in reltimer")
-  #define RESOLUTION_TIMER { \
-      /* converts back */ \
-      if (count==0) i = 4; \
-      else i = count/16384; \
-      j = (count%16384) / 16; \
-    j = (j*1953125) / 2; \
-      /* set timer resolution */ \
-      tval.it_value.tv_sec = (long)i; \
-      tval.it_value.tv_nsec = (long)j; \
-      tval.it_interval.tv_sec = (long)i; \
-      tval.it_interval.tv_nsec = (long)j; \
-  }
+static void timer_stop(void) {
+  tval.it_value.tv_sec = 0L;
+  tval.it_value.tv_nsec = 0L;
+  if (timer_settime(timer, 0, &tval, NULL) )
+    nl_error(3, "Error %d stopping timer", errno );
+}
+
+static void timer_start( unsigned int count) {
+  int i, j;
+
+  /* converts back */
+  if (count==0) i = 4;
+  else i = count/16384;
+  j = (count%16384) / 16;
+  j = (j*1953125) / 2;
+  /* set timer resolution */
+  tval.it_value.tv_sec = (long)i;
+  tval.it_value.tv_nsec = (long)j;
+  tval.it_interval.tv_sec = (long)i;
+  tval.it_interval.tv_nsec = (long)j;
+  if (timer_settime(timer, 0, &tval, NULL) )
+    nl_error(3, "Error %d starting timer", errno );
+}
   
 static void timer_attach(int coid, int code, int value) {
   struct sigevent event;
@@ -80,8 +80,8 @@ static void timer_detach(void) {
 }
 
 /* defines */
-#define HDR "sol"
-#define OPT_MINE "qd:"
+// #define HDR "sol"
+// #define OPT_MINE "qd:"
 #define ST_NO_MODE 0
 #define ST_NEW_MODE 1
 #define ST_MODE 2
@@ -90,9 +90,9 @@ static void timer_detach(void) {
 #define CACHE_ADDR 0x1000
 
 /* function declarations */
-void waitmsg(int);
+static void waitmsg(int, int);
 void my_signalfunction(int sig_number) {
-    DETACH_TIMER;
+    timer_detach();
     exit(0);
 }
 
@@ -101,6 +101,7 @@ static char rcsid[]="$Id$";
 int state;
 int mode_request;
 int crnt_mode, new_mode;
+int Quit = 0;
 // unsigned char dct = DCT_SOLDRV_A;
 // pid_t *proxy_pids;
 unsigned char reg_which;
@@ -150,11 +151,11 @@ static void close_cmd_fd(void) {
   close(cmd_fd);
 }
 
-static void open_cmd_fd( int coid, short code, int value ) {
+static void open_cmd_fd( int coid, short code, int value, char which_sol ) {
+  char cmdbase[12] = "cmd/SoldrvA";
   int old_response = set_response(0);
-  char *cmddev = tm_dev_name("cmd/idx64");
-  close_cmd_fd();
-  cmd_fd = tm_open_name(cmddev, NULL, O_RDONLY|O_NONBLOCK);
+  cmdbase[10] = which_sol;
+  cmd_fd = tm_open_name(tm_dev_name(cmdbase), NULL, O_RDONLY|O_NONBLOCK);
   set_response(old_response);
   if ( cmd_fd < 0 ) {
     nl_error(3, "Unable to open command channel" );
@@ -167,29 +168,6 @@ static void open_cmd_fd( int coid, short code, int value ) {
   cmd_event.sigev_code = code;
   cmd_event.sigev_value.sival_int = value;
 }
-
-static void close_cmd_fd(void) {
-  close(cmd_fd);
-}
-
-static void open_cmd_fd( int coid, short code, int value ) {
-  int old_response = set_response(0);
-  char *cmddev = tm_dev_name("cmd/idx64");
-  close_cmd_fd();
-  cmd_fd = tm_open_name(cmddev, NULL, O_RDONLY|O_NONBLOCK);
-  set_response(old_response);
-  if ( cmd_fd < 0 ) {
-    nl_error(3, "Unable to open command channel" );
-  }
-
-  /* Initialize cmd event */
-  cmd_event.sigev_notify = SIGEV_PULSE;
-  cmd_event.sigev_coid = coid;
-  cmd_event.sigev_priority = SIGEV_PULSE_PRIO_INHERIT;
-  cmd_event.sigev_code = code;
-  cmd_event.sigev_value.sival_int = value;
-}
-
 
 void main(int argc, char **argv) {
 
@@ -272,7 +250,7 @@ void main(int argc, char **argv) {
     nl_error(4, "Proxies not implemented yet" );
 
   /* program code */
-  for (;;) {
+  while (!Quit) {
     switch (state) {
       case ST_NO_MODE:
         /* wait for a msg from anyone except timer */
@@ -317,9 +295,7 @@ void main(int argc, char **argv) {
               case SOL_SET_TIME:
                 /* resolution and set timer going */
                 count = mode_code[ip+1] + (mode_code[ip+2] << 8);
-                RESOLUTION_TIMER;
-                /* up timer */
-                UP_TIMER;
+                timer_start(count);
                 ip += 3;
                 break;
               case SOL_GOTO:
@@ -327,7 +303,7 @@ void main(int argc, char **argv) {
                 break;
               case SOL_END_MODE:
                 state = ST_NO_MODE;
-                DOWN_TIMER;
+                timer_stop();
                 msg(MSG,"Installed");
                 break;
               case SOL_WAITS:
@@ -336,7 +312,7 @@ void main(int argc, char **argv) {
                 ip += 2;
                 break;
               case SOL_SELECT:
-                DOWN_TIMER;
+                timer_stop();
                 if (mode_indices[mode_code[ip+1]]!=-1) {
                   state = ST_NEW_MODE;
                   new_mode = mode_code[ip+1];
@@ -348,7 +324,7 @@ void main(int argc, char **argv) {
                 break;
               case SOL_MSWOK:
                 if (mode_request != 0) {
-                  DOWN_TIMER;
+                  timer_stop();
                   state = ST_NEW_MODE;
                 } else ip++;
                 break;
@@ -387,21 +363,69 @@ void main(int argc, char **argv) {
             waitmsg(1, chid);
             msg(MSG_DEBUG,"Step %d", ip);
             if (--count <= 0) mode_mode = ST_IN_MODE;
+            break;
         }				/* switch */
+        break;
     }				/* switch */
   }				/* for(;;) */
 
 }				/* main */
 
-/*
-Wait for msg from 'invite'. Typically from cmdctrl, timer or processes
-(re-)setting proxies.
-Waiting for msg from invite=0 and recieving timer msg is an error.
-*/
+/**
+  * @return 0 anytime anything significant happens
+  * Command Syntax:
+  *   Empty command is quit
+  *  S%d\n Select Mode n
+  *  
+  * I may just eliminate the Set and Reset Proxy commands
+  * and just have soldrv send directly to command server.
+  */
+  
+static int parse_command( char *buf, int nb ) {
+  int val = 0;
+  char *s = buf;
+  
+  buf[nb] = '\0';
+  switch (*s) {
+    case 'S': /* Select Mode */
+      if (isdigit(*++s)) {
+        while (isdigit(*s)) {
+          val = val*10 + *s - '0';
+        }
+        if ( *s == '\n' || *s == '\0' ) {
+          /* command syntax is OK. Now does it require immediate action? */
+          if (val == 0) {
+            timer_stop();
+            // invite=0; ### check this out
+            if (mode_indices[0] == -1) state = ST_NO_MODE;
+            else {
+              state = ST_NEW_MODE;
+              new_mode = 0;
+            }
+            return 0; // Yes, act on it.
+          } else {
+            new_mode = val;
+            if (state == ST_NO_MODE) {
+              state = ST_NEW_MODE;
+              return 0;
+            } else {
+              mode_request = 1;
+              msg(MSG,"Mode %d: %d Pending",crnt_mode, new_mode);
+              return 1;
+            }
+          }
+        }
+      }
+      break;
+    default: break;
+  }
+  msg(2, "Invalid command: '%s'", buf);
+  return 1;
+}
 
-reply_type reply_back(pid_t id, reply_type r, void *s);
-
-/* read_cmd() returns 1 on any errors, 0 otherwise */
+/**
+  * @return 0 anytime anything significant happens
+  */
 static int read_cmd(void) {
   char cmd[MAX_SOL_CMD];
   int errs = 0;
@@ -412,8 +436,10 @@ static int read_cmd(void) {
     if ( rv > 0 ) {
       buf[rv] = '\0';
       errs = parse_command( buf, rv );
-    } else if ( rv == 0 ) return 1; // quit command
-    else if ( rv == -1 ) {
+    } else if ( rv == 0 ) {
+      Quit = 1;
+      return 0;
+    } else if ( rv == -1 ) {
       if ( errno != EAGAIN )
         nl_error(3, "Received error %d from read() in check_command()", errno );
     } else nl_error(4, "Bad return value [%d] from read in check_command", rv );
@@ -424,87 +450,8 @@ static int read_cmd(void) {
   }
   return errs;
 }
-  switch (sol_msg.msg_hdr) {
-    case DASCMD:
-      if (sol_msg.u.dasc_msg.type != dct ||
-        sol_msg.u.dasc_msg.val >= n_modes ||
-          (mode_indices[sol_msg.u.dasc_msg.val] == -1
-            && sol_msg.u.dasc_msg.val != 0) ) {
-        /* bad received msg */
-        msg(MSG_WARN,"bad received DASCmd msg type %d val %d",sol_msg.u.dasc_msg.type,sol_msg.u.dasc_msg.val);
-        replymsg = reply_back(recv_pid,DAS_UNKN,0);
-      }
-      else {
-        /* good received msg */
-        replymsg = reply_back(recv_pid,DAS_OK,0);
-        if (sol_msg.u.dasc_msg.val == 0) {
-          DOWN_TIMER;
-          invite=0;
-          if (mode_indices[0] == -1) state = ST_NO_MODE;
-          else {
-            state = ST_NEW_MODE;
-            new_mode = 0;
-          }
-        } else {
-          new_mode = sol_msg.u.dasc_msg.val;
-          if (state == ST_NO_MODE) state = ST_NEW_MODE;
-          else {
-            mode_request = 1;
-            msg(MSG,"Mode %d: %d Pending",crnt_mode, new_mode);
-          }
-        }
-      }
-      break;
-    default:
-      if (sol_msg.msg_hdr==reg_which)
-        switch (sol_msg.u.proxy_reg.set_or_reset) {
-          case SOL_SET_PROXY:
-            for (i = 0; i < n_proxies; i++) {
-              if (proxy_ids[i] == sol_msg.u.proxy_reg.proxy_id) {
-                if (proxy_pids[i] == 0) {
-                  proxy_pids[i] = sol_msg.u.proxy_reg.proxy_pid;
-                  replymsg = reply_back(recv_pid, DAS_OK, 0);
-                } else {
-                  msg(MSG_WARN,"proxy id %d already in use to proxy %d",proxy_ids[i],proxy_pids[i]);
-                  replymsg = reply_back(recv_pid, DAS_BUSY, 0);
-                }
-                break;
-              }
-            }
-            if (i>=n_proxies) {
-              msg(MSG_WARN,"no such proxy id %d",sol_msg.u.proxy_reg.proxy_id);
-              replymsg = reply_back(recv_pid,DAS_UNKN, 0);
-            }
-            break;
-          case SOL_RESET_PROXY:
-            for (i = 0; i < n_proxies; i++) {
-              if (proxy_ids[i] == sol_msg.u.proxy_reg.proxy_id) {
-                if (proxy_pids[i] == 0) {
-                  msg(MSG_WARN,"proxy id %d not in use",sol_msg.u.proxy_reg.proxy_id);
-                  replymsg = reply_back(recv_pid,DAS_UNKN, &sol_msg);
-                }
-                else {
-                  sol_msg.u.proxy_reg.proxy_pid = proxy_pids[i];
-                  proxy_pids[i] = 0;
-                  replymsg = reply_back(recv_pid, DAS_OK, &sol_msg);
-                }
-                break;
-              }
-            }
-            if (i>=n_proxies) {
-              msg(MSG_WARN,"no such proxy id %d",sol_msg.u.proxy_reg.proxy_id);
-              replymsg = reply_back(recv_pid,DAS_UNKN,&sol_msg);
-            }
-            break;
-        } /* switch */
-        else {
-          msg(MSG_WARN,"Unknown msg received with header %d",sol_msg.msg_hdr);
-          replymsg = reply_back(recv_pid,DAS_UNKN,0);
-        }
-  } /* switch */
-}
 
-void waitmsg ( int timer_expected, int chid ) {
+static void waitmsg ( int timer_expected, int chid ) {
   int i, rv;
   struct _pulse pulse;
 
@@ -518,27 +465,5 @@ void waitmsg ( int timer_expected, int chid ) {
       rv = timer_expected ? 1 : 0;
       if (!timer_expected) msg(MSG_WARN, "Unexpected timer pulse");
     } else msg(MSG_EXIT_ABNORM,"Unexpected pulse code: %d", pulse.code);
-  } while (rv == 1);
-}
-
-/* reply routine */
-reply_type reply_back(pid_t id, reply_type rep, void *s) {
-  char str[25]="";
-  switch(rep) {
-      case DAS_UNKN: strcpy(str,"UNKNOWN"); break;
-      case DAS_BUSY: strcpy(str,"BUSY");	break;
-      case DAS_OK: break;
-      default: sprintf(str,"undefined reply code %u",rep);
-  }
-  if (strlen(str)) msg(MSG_WARN,"replying %s to task %d",str,id);
-
-  /* this is for norton's convenience */
-  if (s) {
-    memcpy(s,&rep,sizeof(reply_type));
-    if (Reply(id, s, sizeof(reply_type) + sizeof(proxy_reg_type))==-1)
-      msg(MSG_WARN,"error replying to task %d",id);
-  } else if (Reply(id, &rep, sizeof(reply_type))==-1)
-    msg(MSG_WARN,"error replying to task %d",id);
-
-  return(rep);
+  } while (rv == 1 && Quit == 0);
 }
