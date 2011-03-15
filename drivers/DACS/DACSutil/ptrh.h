f8fc7d2f26a59b1800b6901bc9a278bf5d0d65a1
Norton Allen <allen@huarp.harvard.edu>
2011-03-15 21:53:09 +0000

    Add collection functions

--- drivers/DACS/DACSutil/ptrh.h
+++ drivers/DACS/DACSutil/ptrh.h
@@ -11,8 +11,14 @@ class ptrh {
     ptrh( const char *id, USHRT base, USHRT C1, USHRT C2,
 	  USHRT C3, USHRT C4, USHRT C5, USHRT C6);
     void check_coeffs();
+    inline USHRT SHT21T() { return sbrwa(base_addr+2); }
+    inline USHRT SHT21RH() { return sbrwa(base_addr+4); }
     double SHT21T( USHRT S1 );
     double SHT21RH( USHRT S2 );
+    inline USHRT MS5607Ta() { return sbrwa(base_addr+0x16); }
+    inline unsigned char MS5607Tb() { return sbrba(base_addr+0x18); }
+    inline USHRT MS5607Pa() { return sbrwa(base_addr+0x12); }
+    inline unsigned char MS5607Pb() { return sbrba(base_addr+0x14); }
     double MS5607T( USHRT Dta, USHRT Dtb);
     double MS5607P( USHRT Dpa, USHRT Dpb);
   private:
