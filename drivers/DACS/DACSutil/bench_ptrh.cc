ba91ea9efa28d9045ca90647ad2fbc8ec2c44eaf
Norton Allen <allen@huarp.harvard.edu>
2011-03-15 21:17:45 +0000

    Put realistic values in for sample

--- drivers/DACS/DACSutil/bench_ptrh.cc
+++ drivers/DACS/DACSutil/bench_ptrh.cc
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "ptrh.h"
 
-ptrh ptrhA = ptrh( "DACS", 0x300, 0xFFFF, 0x9E96, 0x7106, 0x673E, 0x80F5, 0x6C35 );
+ptrh ptrhA = ptrh( "DACS", 0x300, 0xB28E, 0x9548, 0x6EE9, 0x610A, 0x812C, 0x6C80 );
 
 int main( int argc, char **argv ) {
   double Ta, RH, Tb, P;
-  Ta = ptrhA.SHT21T(0x6768);
-  RH = ptrhA.SHT21RH(0x2772);
-  Tb = ptrhA.MS5607T(0xD9FE, 0x0082);
-  P = ptrhA.MS5607P(0xA4F2, 0x005A);
+  Ta = ptrhA.SHT21T(0x61C4);
+  RH = ptrhA.SHT21RH(0x5D86);
+  Tb = ptrhA.MS5607T(0x62E0, 0x0081);
+  P = ptrhA.MS5607P(0x38FE, 0x0059);
   printf("Ta = %.2lf\n", Ta);
   printf("RH = %.2lf\n", RH);
   printf("Tb = %.2lf\n", Tb);
