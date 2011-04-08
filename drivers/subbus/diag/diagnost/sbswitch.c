be828f8ebdfa7c2d812970c3ac06d8a6e1e90d45
Norton Allen <allen@huarp.harvard.edu>
2011-04-08 22:47:48 +0000

    Add masking functionality

--- drivers/subbus/diag/diagnost/sbswitch.c
+++ drivers/subbus/diag/diagnost/sbswitch.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "nortlib.h"
 #include "subbus.h"
 
 #ifdef __USAGE
-%C
+%C	[mask]
 	Reads the switches from the system controller
-	and writes the result as a single hex digit.
+	and writes the result as two hex digits.
+	The mask is specified using standard C syntax,
+	and the default value is 0xF. Unmasked bits
+	are set to 1 (flight mode!)
 #endif
 
-int main( void ) {
-  unsigned int i;
+int main( int argc, char **argv ) {
+  unsigned short sw;
+  unsigned short mask = 0xF;
+
+  if ( argc > 1 ) {
+    unsigned long tmp_mask = strtoul(argv[1],NULL,0);
+    mask = (unsigned short)tmp_mask;
+  }
+
   if ( load_subbus() == 0 )
 	nl_error( 3, "Unable to load subbus library" );
-  i = read_switches() & 0xF;
-  printf( "%X\n", i );
+  sw = ((read_switches() & mask) | ~mask) & 0xFF;
+  printf( "%02X\n", sw );
   return 0;
 }
