2d97f465699e3113300d6785546c27b02ba54cdc
Norton Allen <allen@huarp.harvard.edu>
2011-07-20 15:21:43 +0000

    Add a comment

--- phrtg/src/trend.cc
+++ phrtg/src/trend.cc
@@ -3,6 +3,8 @@
  */
 #include "ablibs.h"
 #include "phrtg.h"
+#include "nortlib.h"
 
 void RTG_Variable_Trend::Incoming(const char *cmd) {
+  nl_error(0, "RTG_Variable_Trend::Income(\"%s\")", cmd);
 }
