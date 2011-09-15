97404611eb93c04730ecd2992def5770902f1300
Norton Allen <allen@huarp.harvard.edu>
2011-09-15 14:32:11 +0000

    Come to terms with attributes

--- drivers/subbus/diag/diagsuite/cfg.c
+++ drivers/subbus/diag/diagsuite/cfg.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ncurses/curses.h>
+#include "cfg.h"
 #ifdef __QNX__
   #if defined(__QNXNTO__)
     #include "port.h"
@@ -10,19 +11,28 @@
   #endif
 #endif
 
-int init_attrs(char *fname, char *attributes, int max) {
+int init_attrs(char *fname, int *attributes, int max) {
   FILE *fp;
   char buf[20];
   int i;
 
-  for ( i = 0; i < max; i++ )
-    attributes[i] = A_NORMAL;
+  for ( i = 0; i < max; i++ ) {
+    attributes[i] = A_NORMAL | COLOR_PAIR(i+1);
+    init_pair(i+1, COLOR_WHITE, COLOR_BLACK );
+  }
   i = 0;
-  fp = fopene(fname, "rb",0);
+  fp = fopen(fname, "rb");
   if (fp != NULL) {
     for (; i < max; i++) {
+      int attr, ifg, ibg;
+      short fg, bg;
       if ( fgets(buf, 20, fp) == NULL ) break;
-      attributes[i] = atoi(buf);
+      if ( sscanf(buf, "%d%d%d", &attr, &ifg, &ibg) == 3 ) {
+        attributes[i] = (attr & ~A_COLOR) | COLOR_PAIR(i+1);
+        fg = ifg % COLORS;
+        bg = ibg % COLORS;
+        init_pair( i+1, fg, bg );
+      }
     }
     fclose(fp); return(1);
   }
@@ -30,12 +40,16 @@ int init_attrs(char *fname, char *attributes, int max) {
 }
 
 
-int save_attrs(char *name, char *attributes, int max) {
+int save_attrs(char *name, int *attributes, int max) {
   FILE *fp;
   int i;
   fp = fopen(name, "wb");
   if (fp == NULL) return(0);
-  for (i = 0; i < max; i++) fprintf(fp, "%d\n", attributes[i]);
+  for (i = 0; i < max; i++) {
+    short foreground, background;
+    pair_content(i+1, &foreground, &background);
+    fprintf(fp, "%d %d %d\n", attributes[i] & ~A_COLOR, foreground, background);
+  }
   fclose(fp);
   return(1);
 }
--- drivers/subbus/diag/diagsuite/sccfg/sccfg.c
+++ drivers/subbus/diag/diagsuite/sccfg/sccfg.c
@@ -1,15 +1,50 @@
 /* sccfg to initialize attributes */
-#include <curses.h>
-#include <cfg.h>
+#include <ncurses/curses.h>
+#include "cfg.h"
 #include "attribut.h"
 
 #define ESCAPE 27
-unsigned char attributes[MAX_ATTRS];
+int attributes[MAX_ATTRS];
+
+#ifndef VERT_SINGLE
+  #define VERT_SINGLE ACS_VLINE
+#endif
+#ifndef HORIZ_SINGLE
+  #define HORIZ_SINGLE ACS_HLINE
+#endif
+
+#ifndef VERT_DOUBLE
+  #define VERT_DOUBLE VERT_SINGLE
+#endif
+#ifndef HORIZ_DOUBLE
+  #define HORIZ_DOUBLE HORIZ_SINGLE
+#endif
+
+#ifndef KEY_PGDN
+  #define KEY_PGDN KEY_NPAGE
+#endif
+#ifndef KEY_PGUP
+  #define KEY_PGUP KEY_PPAGE
+#endif
+
+
+void report_info(void) {
+  char buf[80];
+  snprintf( buf, 80, "COLORS is %d\n", COLORS );
+  addstr(buf);
+  snprintf( buf, 80, "COLOR_PAIRS is %d\n", COLOR_PAIRS );
+  addstr(buf);
+  snprintf( buf, 80, "%s color and %s change color\n",
+    has_colors() ? "Has" : "Does not have",
+    can_change_color() ? "can" : "cannot" );
+  addstr(buf);
+}
 
 void get_h(void) {
   FILE *fp;
   int c;
 
+  report_info();
   fp = fopen("attribut.h", "r");
   if (fp == NULL) return;
   for (;;) {
@@ -21,12 +56,13 @@ void get_h(void) {
 }
 
 void main(int argc, char **argv) {
-  int i, c, foreground, background;
+  int i, c;
+  short foreground, background;
   WINDOW *testw;
 
-  init_attrs(CFG_FILE,attributes,MAX_ATTRS);
   initscr();
-  attrset(0x30);
+  start_color();
+  init_attrs(CFG_FILE,attributes,MAX_ATTRS);
   clear();
   get_h();
   refresh();
@@ -34,56 +70,86 @@ void main(int argc, char **argv) {
   keypad(stdscr, TRUE);
   testw = newwin(10, 40, 7, 40);
   i = 0;
-  foreground = attributes[i] & 0xF;
-  background = (attributes[i] >> 4) & 0xF;
+  pair_content(i+1, &foreground, &background);
   for (;;) {
     wattrset(testw, attributes[i]);
+    wbkgdset(testw, attributes[i]);
     wclear(testw);
     box(testw, VERT_DOUBLE, HORIZ_DOUBLE);
-    mvwprintw(testw, 5, 10, "Attribute %d", i);
+    mvwprintw(testw, 5, 5, "Attribute %d, fg: %d bg: %d", i, foreground, background);
+    mvwprintw(testw, 6, 7, "attr: %X", attributes[i] );
     wrefresh(testw);
     for (;;) {
       c = wgetch(stdscr);
       switch (c) {
-	case KEY_PGDN:
-	  if (++i == MAX_ATTRS) i = 0;
-	  break;
+        case KEY_PGDN:
+          if (++i == MAX_ATTRS) i = 0;
+          break;
         case KEY_PGUP:
-	  if (--i < 0) i = MAX_ATTRS-1;
-	  break;
-	case KEY_LEFT:
-	  if (--foreground < 0) foreground = 0xF;
-	  if (foreground == 0 && background == 0) foreground = 0xF;
-	  break;
-	case KEY_RIGHT:
-	  if (++foreground == 0x10) foreground = 0;
-	  if (foreground == 0 && background == 0) foreground = 1;
-	  break;
-	case KEY_UP:
-	  background = (background-1) & 0xF;
-	  break;
-	case KEY_DOWN:
-	  background = (background+1) & 0xF;
-	  break;
+          if (--i < 0) i = MAX_ATTRS-1;
+          break;
+        case KEY_LEFT:
+          if (--foreground < 0) foreground = COLORS-1;
+          if (foreground == background && --foreground < 0)
+            foreground = COLORS-1;
+          break;
+        case KEY_RIGHT:
+          if (++foreground == COLORS) foreground = 0;
+          if (foreground == background && ++foreground == COLORS)
+            foreground = 0;
+          break;
+        case KEY_UP:
+          if ( --background < 0 ) background = COLORS-1;
+          if ( foreground == background && --background < 0 )
+            background = COLORS-1;
+          break;
+        case KEY_DOWN:
+          if ( ++background == COLORS ) background = 0;
+          if ( foreground == background && ++background == COLORS )
+            background = 0;
+          break;
+        case 'b':
+        case 'B':
+          attributes[i] ^= A_BOLD;
+          break;
+        case 'd':
+        case 'D':
+          attributes[i] ^= A_DIM;
+          break;
+        case 'k':
+        case 'K':
+          attributes[i] ^= A_BLINK;
+          break;
+        case 'r':
+        case 'R':
+          attributes[i] ^= A_REVERSE;
+          break;
+        case 's':
+        case 'S':
+          attributes[i] ^= A_STANDOUT;
+          break;
+        case 'u':
+        case 'U':
+          attributes[i] ^= A_UNDERLINE;
+          break;
         case ESCAPE:
-	  break;
-	default:
-	  continue;
+          break;
+        default:
+          continue;
       }
       switch (c) {
         case KEY_PGUP:
-	case KEY_PGDN:
-	  foreground = attributes[i] & 0xF;
-	  background = (attributes[i] >> 4) & 0xF;
-	  break;
-	case KEY_LEFT:
-	case KEY_RIGHT:
-	case KEY_UP:
-	case KEY_DOWN:
-	  attributes[i] = (background << 4) + foreground;
-	  break;
-	default:
-	  break;
+        case KEY_PGDN:
+          pair_content(i+1, &foreground, &background);
+          break;
+        case KEY_LEFT:
+        case KEY_RIGHT:
+        case KEY_UP:
+        case KEY_DOWN:
+          init_pair(i+1, foreground, background );
+          break;
+        default:
+          break;
       }
       break;
     }
