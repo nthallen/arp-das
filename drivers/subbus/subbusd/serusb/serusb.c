88707f7e24eb01ef5eebc629f5aff83650271d65
Norton Allen <allen@huarp.harvard.edu>
2012-03-23 01:06:22 +0000

    ascii_escape() moved to nortlib

--- drivers/subbus/subbusd/serusb/serusb.c
+++ drivers/subbus/subbusd/serusb/serusb.c
@@ -39,38 +39,6 @@ static void set_timeout( int enable ) {
   }
 }
 
-#define ESC_BUF_SIZE 80
-static char *ascii_escape(const char *ibuf) {
-  static char ebuf[ESC_BUF_SIZE+3];
-  int ix = 0, ox = 0;
-  while (ibuf[ix] != '\0' && ox < ESC_BUF_SIZE ) {
-    char c = ibuf[ix++];
-    if ( isprint(c) ) {
-      ebuf[ox++] = c;
-    } else {
-      switch ( c ) {
-        case '\n':
-          ebuf[ox++] = '\\';
-          ebuf[ox++] = 'n';
-          break;
-        case '\r':
-          ebuf[ox++] = '\\';
-          ebuf[ox++] = 'r';
-          break;
-        case '\t':
-          ebuf[ox++] = '\\';
-          ebuf[ox++] = 't';
-          break;
-        default:
-          ox += snprintf( ebuf+ox, 4, "\\x%02x", c);
-          break;
-      }
-    }
-  }
-  ebuf[ox] = '\0';
-  return ebuf;
-}
-
 // Transmits a request if the currently queued
 // request has not been transmitted.
 static void process_request(void) {
