37891c4b0251f6f5856bb41ff619f6c789ab143c
Norton Allen <allen@huarp.harvard.edu>
2019-11-15 14:33:49 -0500

    mksflow: forgot one status value

--- drivers/mksflow/mksflow.h
+++ drivers/mksflow/mksflow.h
@@ -20,14 +20,15 @@ typedef struct __attribute__((packed)) {
 #define MKS_STAT_E   0x0004
 #define MKS_STAT_H   0x0008
 #define MKS_STAT_HH  0x0010
-#define MKS_STAT_L   0x0020
-#define MKS_STAT_LL  0x0040
-#define MKS_STAT_M   0x0080
-#define MKS_STAT_OC  0x0100
-#define MKS_STAT_P   0x0200
-#define MKS_STAT_T   0x0400
-#define MKS_STAT_U   0x0800
-#define MKS_STAT_V   0x1000
+#define MKS_STAT_IP  0x0020
+#define MKS_STAT_L   0x0040
+#define MKS_STAT_LL  0x0080
+#define MKS_STAT_M   0x0100
+#define MKS_STAT_OC  0x0200
+#define MKS_STAT_P   0x0400
+#define MKS_STAT_T   0x0800
+#define MKS_STAT_U   0x1000
+#define MKS_STAT_V   0x2000
 
 typedef struct __attribute__((packed)) {
   mks_drive_t drive[MKS_MAX_DRIVES];
