650c4dbb8608ffb75e3b2d59d5e03a3bb79db249
miles <miles@huarp.harvard.edu>
2020-04-09 10:52:59 -0400

    nctabledev: edits for ncurses line drawings

--- tmphlib/src/nctable.c
+++ tmphlib/src/nctable.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <ncurses.h>
 #include <unistd.h>
 #include "nortlib.h"
@@ -120,6 +121,7 @@ int nct_init( const char *winname, int n_rows, int n_cols ) {
   curs_set(0);
   if ( n_scrs == 0 )
     atexit( &nct_shutdown );
+  init_grphchar();
   return n_scrs++;
 }
 
@@ -162,88 +164,94 @@ void nct_clear( int winnum ) {
   mark_dirty();
 }
 
-static unsigned char grphchar[81] = {
-  '\x20', /*  0 = 0000 */
-  '\xB3', /*  1 = 0001 */
-  '\xBA', /*  2 = 0002 */
-  '\xB3', /*  3 = 0010 */
-  '\xB3', /*  4 = 0011 */
-  '\xBA', /*  5 = 0012 */
-  '\xBA', /*  6 = 0020 */
-  '\xBA', /*  7 = 0021 */
-  '\xBA', /*  8 = 0022 */
-  '\xC4', /*  9 = 0100 */
-  '\xDA', /* 10 = 0101 */
-  '\xD6', /* 11 = 0102 */
-  '\xC0', /* 12 = 0110 */
-  '\xC3', /* 13 = 0111 */
-  '\xD6', /* 14 = 0112 */
-  '\xD3', /* 15 = 0120 */
-  '\xD3', /* 16 = 0121 */
-  '\xC7', /* 17 = 0122 */
-  '\xCD', /* 18 = 0200 */
-  '\xD5', /* 19 = 0201 */
-  '\xC9', /* 20 = 0202 */
-  '\xD4', /* 21 = 0210 */
-  '\xC6', /* 22 = 0211 */
-  '\xC9', /* 23 = 0212 */
-  '\xC8', /* 24 = 0220 */
-  '\xC8', /* 25 = 0221 */
-  '\xCC', /* 26 = 0222 */
-  '\xC4', /* 27 = 1000 */
-  '\xBF', /* 28 = 1001 */
-  '\xB7', /* 29 = 1002 */
-  '\xD9', /* 30 = 1010 */
-  '\xB4', /* 31 = 1011 */
-  '\xB7', /* 32 = 1012 */
-  '\xBD', /* 33 = 1020 */
-  '\xBD', /* 34 = 1021 */
-  '\xB6', /* 35 = 1022 */
-  '\xC4', /* 36 = 1100 */
-  '\xC2', /* 37 = 1101 */
-  '\xD2', /* 38 = 1102 */
-  '\xC1', /* 39 = 1110 */
-  '\xC5', /* 40 = 1111 */
-  '\xD2', /* 41 = 1112 */
-  '\xD0', /* 42 = 1120 */
-  '\xD0', /* 43 = 1121 */
-  '\xD7', /* 44 = 1122 */
-  '\xCD', /* 45 = 1200 */
-  '\x20', /* 46 = 1201 */
-  '\x20', /* 47 = 1202 */
-  '\x20', /* 48 = 1210 */
-  '\x20', /* 49 = 1211 */
-  '\x20', /* 50 = 1212 */
-  '\x20', /* 51 = 1220 */
-  '\x20', /* 52 = 1221 */
-  '\x20', /* 53 = 1222 */
-  '\xCD', /* 54 = 2000 */
-  '\xB8', /* 55 = 2001 */
-  '\xBB', /* 56 = 2002 */
-  '\xBE', /* 57 = 2010 */
-  '\xB5', /* 58 = 2011 */
-  '\x20', /* 59 = 2012 */
-  '\xBC', /* 60 = 2020 */
-  '\x20', /* 61 = 2021 */
-  '\xB9', /* 62 = 2022 */
-  '\x20', /* 63 = 2100 */
-  '\x20', /* 64 = 2101 */
-  '\x20', /* 65 = 2102 */
-  '\x20', /* 66 = 2110 */
-  '\x20', /* 67 = 2111 */
-  '\x20', /* 68 = 2112 */
-  '\x20', /* 69 = 2120 */
-  '\x20', /* 70 = 2121 */
-  '\x20', /* 71 = 2122 */
-  '\xCD', /* 72 = 2200 */
-  '\xD1', /* 73 = 2201 */
-  '\xCB', /* 74 = 2202 */
-  '\xCF', /* 75 = 2210 */
-  '\xD8', /* 76 = 2211 */
-  '\x20', /* 77 = 2212 */
-  '\xCA', /* 78 = 2220 */
-  '\x20', /* 79 = 2221 */
-  '\xCE'  /* 80 = 2222 */
+static chtype grphchar[81];
+  
+static void init_grphchar() {
+  static bool grphchar_initialized = false;
+  if (!grphchar_initialized) {
+    grphchar_initialized = true;
+    grphchar[0] = ' ';
+    grphchar[1] = ACS_VLINE;
+    grphchar[2] = ACS_VLINE;
+    grphchar[3] = ACS_VLINE;
+    grphchar[4] = ACS_VLINE;
+    grphchar[5] = ACS_VLINE;
+    grphchar[6] = ACS_VLINE;
+    grphchar[7] = ACS_VLINE;
+    grphchar[8] = ACS_VLINE;
+    grphchar[9] = ACS_HLINE;
+    grphchar[10] = ACS_ULCORNER;
+    grphchar[11] = ACS_ULCORNER;
+    grphchar[12] = ACS_LLCORNER;
+    grphchar[13] = ACS_LTEE;
+    grphchar[14] = ACS_LTEE;
+    grphchar[15] = ACS_LLCORNER;
+    grphchar[16] = ACS_LTEE;
+    grphchar[17] = ACS_LTEE;
+    grphchar[18] = ACS_HLINE;
+    grphchar[19] = ACS_ULCORNER;
+    grphchar[20] = ACS_ULCORNER;
+    grphchar[21] = ACS_LLCORNER;
+    grphchar[22] = ACS_LTEE;
+    grphchar[23] = ACS_LTEE;
+    grphchar[24] = ACS_LLCORNER;
+    grphchar[25] = ACS_LTEE;
+    grphchar[26] = ACS_LTEE;
+    grphchar[27] = ACS_HLINE;
+    grphchar[28] = ACS_URCORNER;
+    grphchar[29] = ACS_URCORNER;
+    grphchar[30] = ACS_LRCORNER;
+    grphchar[31] = ACS_RTEE;
+    grphchar[32] = ACS_RTEE;
+    grphchar[33] = ACS_LRCORNER;
+    grphchar[34] = ACS_RTEE;
+    grphchar[35] = ACS_RTEE;
+    grphchar[36] = ACS_HLINE;
+    grphchar[37] = ACS_TTEE;
+    grphchar[38] = ACS_TTEE;
+    grphchar[39] = ACS_BTEE;
+    grphchar[40] = ACS_PLUS;
+    grphchar[41] = ACS_PLUS;
+    grphchar[42] = ACS_BTEE;
+    grphchar[43] = ACS_PLUS;
+    grphchar[44] = ACS_PLUS;
+    grphchar[45] = ACS_HLINE;
+    grphchar[46] = ACS_TTEE;
+    grphchar[47] = ACS_TTEE;
+    grphchar[48] = ACS_BTEE;
+    grphchar[49] = ACS_PLUS;
+    grphchar[50] = ACS_PLUS;
+    grphchar[51] = ACS_BTEE;
+    grphchar[52] = ACS_PLUS;
+    grphchar[53] = ACS_PLUS;
+    grphchar[54] = ACS_HLINE;
+    grphchar[55] = ACS_URCORNER;
+    grphchar[56] = ACS_URCORNER;
+    grphchar[57] = ACS_LRCORNER;
+    grphchar[58] = ACS_RTEE;
+    grphchar[59] = ACS_RTEE;
+    grphchar[60] = ACS_LRCORNER;
+    grphchar[61] = ACS_RTEE;
+    grphchar[62] = ACS_RTEE;
+    grphchar[63] = ACS_HLINE;
+    grphchar[64] = ACS_TTEE;
+    grphchar[65] = ACS_TTEE;
+    grphchar[66] = ACS_BTEE;
+    grphchar[67] = ACS_PLUS;
+    grphchar[68] = ACS_PLUS;
+    grphchar[69] = ACS_BTEE;
+    grphchar[70] = ACS_PLUS;
+    grphchar[71] = ACS_PLUS;
+    grphchar[72] = ACS_HLINE;
+    grphchar[73] = ACS_TTEE;
+    grphchar[74] = ACS_TTEE;
+    grphchar[75] = ACS_BTEE;
+    grphchar[76] = ACS_PLUS;
+    grphchar[77] = ACS_PLUS;
+    grphchar[78] = ACS_BTEE;
+    grphchar[79] = ACS_PLUS;
+    grphchar[80] = ACS_PLUS;
+  }
 };
 
 static unsigned char asciichar[81] = {
@@ -330,7 +338,7 @@ static unsigned char asciichar[81] = {
   '+'  /* 80 = 2222 */
 };
 
-static unsigned char *nct_boxchars = grphchar;
+static chtype *nct_boxchars = grphchar;
 
 void nct_charset(int n) {
   switch (n) {
